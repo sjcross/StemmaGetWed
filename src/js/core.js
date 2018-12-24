a96c13b6ac254e283dc59cf8ddefbd03e5f2eefb
Stephen Cross <stephen.cross@bristol.ac.uk>
2018-12-24 14:49:47 +0000

    Finished non-clickable tiles
    Each tile can now be set to be non-clickable.  This also disables the hover highlighting.
    
    Added example of including a shortcut icon to a tile.

--- src/js/core.js
+++ src/js/core.js
@@ -14,6 +14,7 @@ function shuffle(array) {
   }
 
   return array;
+
 }
 
 function getColour(hue,saturation,lightness) {
---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+var baseHue = 225;
+var baseSaturation = 60;
+
+// Change plain tile colour
+var elements = document.getElementsByClassName("plain-tile");
+for (var i=0;i<elements.length;i++) {
+  elements[i].setAttribute("style","background-color: "+getColour(baseHue,baseSaturation,(50+Math.random()*40)));
+}
+
+// Change plain tile colour
+var elements = document.getElementsByClassName("text-tile");
+for (var i=0;i<elements.length;i++) {
+  elements[i].setAttribute("style","background-color: "+getColour(baseHue,baseSaturation,(70+Math.random()*20)));
+}
+
+// Randomly set tile images
+var folder = "../img/256_tiles/";
+var tileNames = shuffle(["Tile1.png","Tile2.png","Tile3.png"]);
+
+var elements = document.getElementsByClassName("image-tile");
+for (var i=0;i<elements.length;i++) {
+  elements[i].setAttribute("data-cover",folder+tileNames[i]);
+}
