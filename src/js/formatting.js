fa7ee7f6ce56a02e23ce41f5c118f0ff5bc79609
Stephen Cross <stephen.cross@bristol.ac.uk>
2018-12-31 10:07:35 +0000

    Tidied up Javascript functions

--- src/js/formatting.js
+++ src/js/formatting.js
@@ -1,23 +1,29 @@
-var baseHue = 225;
-var baseSaturation = 60;
+const baseHue = 225;
+const baseSaturation = 60;
 
 // Change plain tile colour
-var elements = document.getElementsByClassName("plain-tile");
-for (var i=0;i<elements.length;i++) {
-  elements[i].setAttribute("style","background-color: "+getColour(baseHue,baseSaturation,(50+Math.random()*40)));
-}
+function changeTileColour(baseHue, baseSaturation) {
+  const plainElements = document.getElementsByClassName("plain-tile");
+  for (var i = 0; i < plainElements.length; i++) {
+    plainElements[i].setAttribute("style", "background-color: " + getColour(baseHue, baseSaturation, (50 + Math.random() * 40)));
+  }
 
-// Change plain tile colour
-var elements = document.getElementsByClassName("text-tile");
-for (var i=0;i<elements.length;i++) {
-  elements[i].setAttribute("style","background-color: "+getColour(baseHue,baseSaturation,(70+Math.random()*20)));
+  const textElements = document.getElementsByClassName("text-tile");
+  for (var i = 0; i < textElements.length; i++) {
+    textElements[i].setAttribute("style", "background-color: " + getColour(baseHue, baseSaturation, (70 + Math.random() * 20)));
+  }
 }
 
 // Randomly set tile images
-var folder = "../../img/256_tiles/";
-var tileNames = shuffle(["Tile1.png","Tile2.png","Tile3.png"]);
+function randomiseTileImages() {
+  const folder = "../../img/256_tiles/";
+  const tileNames = shuffle(["Tile1.png", "Tile2.png", "Tile3.png"]);
 
-var elements = document.getElementsByClassName("image-tile");
-for (var i=0;i<elements.length;i++) {
-  elements[i].setAttribute("data-cover",folder+tileNames[i]);
+  const elements = document.getElementsByClassName("image-tile");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].setAttribute("data-cover", folder + tileNames[i]);
+  }
 }
+
+changeTileColour(baseHue,baseSaturation);
+randomiseTileImages();
